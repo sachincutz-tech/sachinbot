a222cff9356bd6cd5c853cf5844b8c4d4feca9aa
NRLajai <nrlajai28@gmail.com>
2025-11-20 00:27:03 +0530

    added sh file

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "================================================>"
+echo $API_ID
+echo $API_HASH
+echo $BOT_TOKEN
+echo $MONGO_URI
+echo $ADMINS
+echo "================================================>"
